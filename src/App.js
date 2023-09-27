279715164479977b797e439d5d6030c0fe04a853
js3617 <a332210@naver.com>
2023-09-27 20:32:46 +0900

    className 선언, style 선언 방식, 변수 가져오는 방법

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+let post = '부산 우동 맛집'; //서버에서 가져온 데이터라고 생각하자
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <p>클래스 명은 무조건 ClassName로 선언해야한다</p>
+        <h3 style={{color : 'red'}}>스타일선언도 무조건 이름:값 을 사용하여 작성한다</h3>
+      </div>
+      <h3>{ post } 변수명을 사용하고싶을때는 "중괄호 변수명 중괄호"방식으로 사용해야한다</h3>
     </div>
   );
 }
