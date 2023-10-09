29cc56e62906d5ebda7aa3fae41c5b22de4bcd91
js3617 <a332210@naver.com>
2023-10-09 20:57:21 +0900

    react 동적인 UI 생성

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import { useState } from 'react';
 function App() {
 
 let post = '부산 우동 맛집';
-let [글제목,b] = useState(['남자코트 추천','파이썬 독학','부산 우동 맛집']);
+let [글제목,글제목변경] = useState(['남자코트 추천','파이썬 독학','부산 우동 맛집']);
 let [따봉, 따봉변경] = useState(0);
+let [modal, setModal] = useState(0);
 
 
   return (
@@ -17,6 +18,13 @@ let [따봉, 따봉변경] = useState(0);
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={ () => {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy);
+        } }>글수정</button>
+
       <div className="list">
         <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
         <p>10월 6일 발행</p>
@@ -28,11 +36,40 @@ let [따봉, 따봉변경] = useState(0);
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h3>{ 글제목[2] }</h3>
+        <h3 onClick={() =>{setModal(!modal)}}>{ 글제목[2] }</h3>
         <p>2월 17일 발행</p>
       </div>
+      {
+        modal == 1 ? <Modal/> : null
+      }
+      <Naver/>
     </div>
   );
 }
+
+
+function Modal(){
+  return(
+    <>
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  )
+}
+
+function Naver(){
+  return(
+    <div>
+      <button>
+        <a href='naver.com'>네이버 바로가기</a>
+      </button>
+    </div>
+  )
+}
+
+
 
 export default App;
