dac97f0582f0f680995d795bab7141fcfd0223b4
js3617 <a332210@naver.com>
2023-11-12 16:16:37 +0900

    map 함수 사용해서 글제목 반복 하기, 따봉 수정

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ function App() {
 
 let post = '부산 우동 맛집';
 let [글제목,글제목변경] = useState(['남자코트 추천','파이썬 독학','부산 우동 맛집']);
-let [따봉, 따봉변경] = useState(0);
+let [따봉, 따봉변경] = useState([0,0,0]);
+// let [따봉1, 따봉변경1] = useState(0);
+// let [따봉2, 따봉변경2] = useState(0);
 let [modal, setModal] = useState(0);
+
 
 
   return (
@@ -25,7 +28,7 @@ let [modal, setModal] = useState(0);
         글제목변경(copy);
         } }>글수정</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h3>{ 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h3>
         <p>10월 6일 발행</p>
       </div>
@@ -38,11 +41,25 @@ let [modal, setModal] = useState(0);
       <div className="list">
         <h3 onClick={() =>{setModal(!modal)}}>{ 글제목[2] }</h3>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        글제목.map(function(a, i){
+          return (
+            <div className='list' key={i}>
+              <h4>{글제목[i]} <span onClick={()=>{ 
+                let copy = [...따봉];
+                copy[i] = copy[i] + 1;
+                따봉변경(copy)
+              }}>👍</span> {따봉[i]} </h4>
+              <p>11월 12일 발행</p>
+            </div>
+          )
+        })
+      }
       {
         modal == 1 ? <Modal/> : null
       }
-      <Naver/>
     </div>
   );
 }
@@ -59,17 +76,5 @@ function Modal(){
     </>
   )
 }
-
-function Naver(){
-  return(
-    <div>
-      <button>
-        <a href='naver.com'>네이버 바로가기</a>
-      </button>
-    </div>
-  )
-}
-
-
 
 export default App;
